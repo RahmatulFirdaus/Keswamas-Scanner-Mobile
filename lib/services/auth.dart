ab880abc753529f20adf4b53873f4bf7e827c326
RahmatulFirdaus <rahmatulfirdausuniska@gmail.com>
2024-11-11 09:52:56 +0800

    update service

---
+++ lib/services/auth.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthService {
+  final storage = FlutterSecureStorage();
+
+  Future<void> login(String username, String password) async {
+    var url = Uri.parse('http://10.0.10.58:3000/api/login');
+    var response = await http.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'password': password}));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final token = data['token'];
+      final refreshToken = data['refresh_token'];
+
+      await storage.write(key: 'jwt_token', value: token);
+      await storage.write(key: 'refresh_token', value: refreshToken);
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+
+  Future<void> refreshJwtToken() async {
+    final refreshToken = await storage.read(key: 'refresh_token');
+
+    if (refreshToken == null) {
+      throw Exception('Refresh token not found');
+    }
+
+    var url = Uri.parse('http://10.0.10.58:3000/api/refreshToken');
+    var response = await http.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'refresh_token': refreshToken}));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final newToken = data['token'];
+
+      await storage.write(key: 'jwt_token', value: newToken);
+    } else {
+      throw Exception('Terjadi Kesalahan');
+    }
+  }
+
+  Future<void> logout() async {
+    await storage.delete(key: 'jwt_token');
+    await storage.delete(key: 'refresh_token');
+  }
+}
