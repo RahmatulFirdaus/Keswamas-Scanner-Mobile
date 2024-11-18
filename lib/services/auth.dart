fe25cf71327b498e49a81232425fd8f46826b9f7
RahmatulFirdaus <rahmatulfirdausuniska@gmail.com>
2024-11-18 09:25:11 +0800

    up

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -6,7 +6,7 @@ class AuthService {
   final storage = const FlutterSecureStorage();
 
   Future<String?> login(String username, String password) async {
-    var url = Uri.parse('http://192.168.1.8:3000/api/login');
+    var url = Uri.parse('http://10.0.10.58:3000/api/login');
     var response = await http.post(url,
         headers: {'Content-Type': 'application/json; charset=UTF-8'},
         body: jsonEncode({'username': username, 'password': password}));
